862e2c3a8db318f5dd92c59cec475403e644c56a
trustill <gonya.kzn@gmail.com>
2026-04-27 01:03:25 +0300

    test: preparing plan query

--- async_main.py
+++ async_main.py
@@ -125,23 +125,26 @@ def user_accepted_tou(query):
     welcome_message(query.message, query.from_user.first_name)
 
 @bot.callback_query_handler(func=lambda x: x.data.startswith('subscribe'))
-def subscribe(query):
-    user_id = query.message.chat.id
+def subscribe_on_service(query):
     try:
+        user_id = query.message.chat.id
         bot.delete_message(chat_id=user_id,
                            message_id=query.message.id)
 
         lang = db_client.get_user_lang(user_id)
         kb = keyboards.get_plans(lang)
     except Exception as e:
+        user_id = None
         lang = None
         kb = None
+        logging.error(e)
         print(e)
     try:
         bot.send_message(chat_id=user_id,
                          text=msg_data[lang]["messages"]["choose_plan"],
                          reply_markup=kb)
     except Exception as e:
+        logging.error(e)
         print(e)
 
 @bot.callback_query_handler(func=lambda x: x.data.startswith('sub:'))
