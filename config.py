39693a76d07e2753b26e16b9ba2fa7d3fb2e194a
trustill <gonya.kzn@gmail.com>
2026-05-04 07:59:14 +0300

    refactor: new config

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-token = "8752956295:AAGqaBNCtr2-Yxye2sYXJAkXBgYuDyEPgwA"
+token = "8724398760:AAFlm1mlbCoUWPNqucFRvLGPy258SxBEXpw"
 payment_token = "d5382da2-d4de-445f-90fb-e0369b1e6d7b"
 conn_str = os.getenv("conn_data")
 # conn_str = "dbname=arrow_guard_main user=postgres password=BineyFimozer host=127.0.0.1 port=5555"
